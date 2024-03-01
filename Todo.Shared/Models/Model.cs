7af7b20c1ba11a82f28304ca71e54b20ac407c4d
RenatoDidier <renatodidierf@gmail.com>
2024-03-01 14:25:18 -0300

    Configuração inicial da API e envio para repositório

---
+++ Todo.Shared/Models/Model.cs
@@ -0,0 +1,11 @@
+﻿namespace Todo.Shared.Models
+{
+    public abstract class Model
+    {
+        public Model()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+        public string Id { get; set; }
+    }
+}
---
+++ Todo.Web/Controllers/TodoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Web.Controllers
+{
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet("/")]
+        public string ChamarApi()
+        {
+            Console.WriteLine("Chamou aqui");
+
+            return "Está funcionando";
+        }
+    }
+}
